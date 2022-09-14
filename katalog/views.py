13a3ffbf8e1aae69d5e887facd0679b1ffa49f18
Astrid Swardhani Putri <astrid.swardhani@ui.ac.id>
2022-09-14 16:52:56 +0700

    push tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+def show_catalog(request):
+    cat_Item_Container= CatalogItem.objects.all()
+    context = {
+    'list_barang': cat_Item_Container,
+    'nama': "Astrid",
+    'NPM' :"2106707113"
+    }
+    return render(request, "katalog.html", context)
