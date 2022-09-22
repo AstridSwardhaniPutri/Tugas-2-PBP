8086c638543dec0e704dae46e24fdec4feda499d
Astrid Swardhani Putri <astrid.swardhani@ui.ac.id>
2022-09-22 10:43:30 +0700

    updating tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestUrl (TestCase):
+    def test_url(self):
+        self.assertEqual(Client().get('/katalog').status_code,200)
